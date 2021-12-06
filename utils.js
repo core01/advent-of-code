d284dcf3ada0243f426ef2a8853e1f5d79e76801
Roman Sadoyan <roman@sadoyan.me>
2021-12-06 22:35:55 +0300

    Day 6

--- utils.js
+++ utils.js
@@ -4,3 +4,4 @@ export const readFile = () => {
     return fs.readFileSync(process.argv[2], 'utf8').split('\n').filter(Boolean);
 }
 
+export const sumReducer = (previousValue, currentValue) => previousValue + currentValue
