539fc00d2329ebcafa1a26c4a28248a5c92907db
Roman Sadoyan <roman@sadoyan.me>
2021-12-07 09:34:08 +0300

    Day 7

---
+++ day7/main.js
@@ -0,0 +1,40 @@
+import {readFile, sumTo} from '../utils.js'
+
+const getFirst = () => {
+    let [inputData] = readFile()
+    let positions = inputData.split(',').map(el => parseInt(el, 10))
+    let res = []
+    positions.forEach(el => {
+        positions.forEach(otherEl => {
+            if (el !== otherEl) {
+                res[el] += Math.abs(el - otherEl)
+            } else {
+                res[el] = 0
+            }
+        })
+    })
+
+    res = res.filter(Boolean)
+    return Math.min(...res)
+}
+
+
+const getSecond = () => {
+    let [inputData] = readFile()
+    let positions = inputData.split(',').map(el => parseInt(el, 10))
+
+    const maxPosition = Math.max(...positions)
+    const distances = Array(maxPosition + 1).fill(Infinity)
+
+    for (let i = 0; i < distances.length; i++) {
+        distances[i] = 0
+        positions.forEach(el => {
+            distances[i] += sumTo(Math.abs(el - i))
+        })
+    }
+
+    return Math.min(...distances);
+}
+
+console.log('First: ', getFirst())
+console.log('Second: ', getSecond())
--- utils.js
+++ utils.js
@@ -1,7 +1,15 @@
-import fs from 'fs';
+import fs from 'fs'
 
 export const readFile = () => {
-    return fs.readFileSync(process.argv[2], 'utf8').split('\n').filter(Boolean);
+    return fs.readFileSync(process.argv[2], 'utf8').split('\n').filter(Boolean)
 }
 
 export const sumReducer = (previousValue, currentValue) => previousValue + currentValue
+
+export const sumTo = (n) => {
+    let sum = 0
+    for (let i = 1; i <= n; i++) {
+        sum += i
+    }
+    return sum
+}
